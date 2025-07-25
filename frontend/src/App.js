6200399d45898be74cdf7395c8b82fd7af97fe2f
Aleksandra Jarczyk <aleksandra.jarczyk@baywa-re.com>
2025-07-25 14:05:17 +0200

    Basic components and bootstrap styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import React from 'react'
+import HomeScreen from './screen/HomeScreen'
 
 const App = () => {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+      
+    </>
   )
 }
 
